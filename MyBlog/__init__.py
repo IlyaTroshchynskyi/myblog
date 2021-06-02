84e0b7a65e9bf05b622d9cb2d3dc8eab627fc479
Ilya <itroshchinskiy@rambler.ua>
2021-06-02 23:31:45 +0300

    added login manager

--- MyBlog/__init__.py
+++ MyBlog/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -12,6 +12,8 @@ db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'something_very_secret'
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
-
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' # need to redirect on this page when we use decorator login required
+login_manager.login_message_category = 'info'
 
 from MyBlog import routes
