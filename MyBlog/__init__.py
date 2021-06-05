3a13124de0020a7d1771090931c20bbcc03e75ba
Ilya <itroshchinskiy@rambler.ua>
2021-06-05 12:52:19 +0300

    restructuring

--- MyBlog/__init__.py
+++ MyBlog/__init__.py
@@ -1,19 +1,37 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from MyBlog.config import Config
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_RECORD_QUERIES'] = True
-db = SQLAlchemy(app)
-app.config['SECRET_KEY'] = 'something_very_secret'
-migrate = Migrate(app, db)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login' # need to redirect on this page when we use decorator login required
+
+db = SQLAlchemy()
+
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login' # need to redirect on this page when we use decorator login required
 login_manager.login_message_category = 'info'
+mail = Mail()
 
-from MyBlog import routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    from MyBlog.users.routes import users
+    from MyBlog.posts.routes import posts
+    from MyBlog.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    migrate = Migrate(app, db)
+
+    return app
