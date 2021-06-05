a29acf6f0cfe0ad657836f87e7d2cb306c7c7e3b
Ilya <itroshchinskiy@rambler.ua>
2021-06-05 13:17:00 +0300

    register new blueprint

--- MyBlog/__init__.py
+++ MyBlog/__init__.py
@@ -9,7 +9,7 @@ from MyBlog.config import Config
 
 
 db = SQLAlchemy()
-
+migrate = Migrate()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login' # need to redirect on this page when we use decorator login required
@@ -25,13 +25,16 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    migrate.init_app(app, db)
+
     from MyBlog.users.routes import users
     from MyBlog.posts.routes import posts
     from MyBlog.main.routes import main
+    from MyBlog.errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
-    migrate = Migrate(app, db)
+    app.register_blueprint(errors)
 
     return app
